09d19c11f37ff9e36330e9fc622a600857239552
Jorge Rodriguez <jarf4321@gmail.com>
2022-07-24 11:39:32 -0500

    Add Promises folder

---
+++ Promises/anotherPromiseExample.js
@@ -0,0 +1,11 @@
+const recordVideoOne = new Promise((resolve, reject) => {
+  resolve("Video 1 Recorded");
+});
+
+const recordVideoTwo = new Promise((resolve, reject) => {
+  resolve("Video 2 Recorded");
+});
+
+const recordVideoThree = new Promise((resolve, reject) => {
+  resolve("Video 3 Recorded");
+});
